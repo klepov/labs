89d404b6f1aac3b6f4f75c9fa35b64f6799ae9fa
dima <dima1da@yandex,ru>
2015-05-07 23:30:18 +0300

    third lab with GUI

---
+++ guiLab3.py
@@ -0,0 +1,173 @@
+__author__ = 'dima'
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+SYMBOLS = SYMBOLS.lower()
+from tkinter import *
+import numpy as np
+#первая координата по y вторая по х
+
+
+
+ALPHABET_FULL = \
+np.array([
+
+# 1    2    3    4    5    6    7    8    9    10   11   12   13   14   15   16   17  18    19   20   21   22   23  24   25   26
+['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ],#1
+['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A' ],#2
+['C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B' ],#3
+['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C' ],#4
+['E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D' ],#5
+['F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E' ],#6
+['G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F' ],#7
+['H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G' ],#8
+['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' ],#9
+['J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' ],#10
+['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' ],#11
+['L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K' ],#12
+['M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L' ],#13
+['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M' ],#14
+['O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N' ],#15
+['P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O' ],#16
+['Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P' ],#17
+['R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q' ],#18
+['S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R' ],#19
+['T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S' ],#20
+['U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T' ],#21
+['V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U' ],#22
+['W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V' ],#23
+['X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W' ],#24
+['Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X' ],#25
+['Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y' ]#26
+])
+
+message = "Click one of the following buttons to open a KBD file in Notepad"
+message = message.lower()
+
+def gcd(a, b):
+    # Return the GCD of a and b using Euclid's Algorithm
+    while a != 0:
+        a, b = b % a, a
+    return b
+
+def findModInverse(a, m):
+    # Returns the modular inverse of a % m, which is
+    # the number x such that a*x % m = 1
+
+    if gcd(a, m) != 1:
+        return None # no mod inverse if a & m aren't relatively prime
+
+    # Calculate using the Extended Euclidean Algorithm:
+    u1, u2, u3 = 1, 0, a
+    v1, v2, v3 = 0, 1, m
+    while v3 != 0:
+        q = u3 // v3 # // is the integer division operator
+        v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
+    return u1 % m
+
+def decryptMessage(keyA,keyB, message):
+    KeyA = keyA
+    KeyB = keyB
+
+    plaintext = ''
+    modInverseOfKeyA = findModInverse(keyA, len(SYMBOLS))
+
+    for symbol in message:
+        if symbol in SYMBOLS:
+            # шифровка
+            symIndex = SYMBOLS.find(symbol)
+            plaintext += SYMBOLS[(symIndex - keyB) * modInverseOfKeyA % len(SYMBOLS)]
+        else:
+            plaintext += symbol
+    return plaintext
+
+
+
+def squareBeaufort(fraza_need):
+    fraza = fraza_need
+    fraza = ''.join(fraza.split())
+    print(fraza," через аффины")
+
+    key = 'key'.lower()
+    key = key * len(fraza)
+
+    resultForByof = []
+
+    for i in range(len(fraza)):
+        find = SYMBOLS.find(fraza[i])
+        if find == 0:
+            find = 26
+        find_key = SYMBOLS.find(key[i])
+        resultForByof.append(ALPHABET_FULL[26 - find][find_key])
+
+        i+=1                                    #Y           X
+
+    resultForByof = ''.join(resultForByof)
+
+
+    return (resultForByof)
+
+
+
+
+
+
+
+def viginer(message, key):
+    Message = message  # исходное сообщение
+    Key = key # ключ
+
+    Key *= len(Message) // len(Key) + 1  # подгоняем ключ
+    result = ''.join([chr((ord(j) + ord(Key[i])) % 26 + ord('A')) for i, j in enumerate(Message)])  # шифруем
+    return (result)
+
+
+
+
+
+class App(Frame):
+    def __init__(self, master):
+        super(App,self).__init__(master)
+        self.grid()
+        self.create_widget()
+
+    def create_widget(self):
+        self.lbl = Label(self, text = "enter pass: ")
+        self.lbl.grid(row = 0,column = 0, columns = 2, sticky = W)
+
+        self.pw_ent = Entry(self)
+        self.pw_ent.grid(row = 0, column = 1, sticky = W)
+
+        self.buttn = Button(self, text = "ok", command = self.reveal)
+        self.buttn.grid(row = 2, column = 0, sticky = W)
+
+        self.buttn2 = Button(self, text = "clear", command = self.clear)
+        self.buttn2.grid(row = 2, column = 1, sticky = W)
+
+        self.secret_txt = Text(self, width = 100, height = 100, wrap = WORD)
+        self.secret_txt.grid(row = 3, column = 0, columnspan = 2, sticky = W)
+
+    def clear(self):
+        self.secret_txt.delete(0.0,END)
+
+    def reveal(self):
+
+        translated = decryptMessage(keyA = 7,keyB = 5, message=message) #аффины
+        beaufort = squareBeaufort(translated)
+        result_pol = viginer(beaufort, "key")
+
+
+        print(beaufort,"через Beaufort")#Beaufort
+        print(result_pol," через вижинер")
+
+
+        self.secret_txt.insert(0.0, result_pol,"\n")
+
+
+
+
+
+root = Tk()
+root.title("lol")
+root.geometry("800x800")
+
+App = App(root)
+root.mainloop()
